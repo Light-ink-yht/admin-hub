e77e51f8193a41b3c2cf30102921059a6b4b7357
yht <1480224563@qq.com>
2025-09-29 23:09:59 +0800

    # 环境变量配置、跨域设置

--- admin-hub-ui/env.d.ts
+++ admin-hub-ui/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+export interface EnvMeta extends Record<string, string>{
+  NODE_ENV: string
+  VITE_APP_TITLE: string
+  VITE_API_BASE_URL: string
+  VITE_API_TIMEOUT: number
+  VITE_APP_PORT: number
+  VITE_APP_SERVER_URL: string
+}
--- admin-hub-ui/vite.config.ts
+++ admin-hub-ui/vite.config.ts
@@ -3,16 +3,32 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { loadEnv } from 'vite'
+import type { EnvMeta } from './env'
+
+const envDir = './'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig((config) => {
+  const env = loadEnv(config.mode, envDir) as EnvMeta
+  console.log('后端地址: ', env.VITE_APP_SERVER_URL)
+  return {
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+    envDir: envDir,
+    server: {
+      host: '0.0.0.0',
+      port: env.VITE_APP_PORT,
+      proxy: {
+        '/api': {
+          target: env.VITE_APP_SERVER_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  }
 })
